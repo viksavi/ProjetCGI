ced6116305e1b5a8083ad8cacf0b3ff720a990cf
viksavi <viktoriia.yarmolich@univ-tlse3.fr>
2025-02-14 08:08:19 +0100

    Changes

--- FramedDreams/src/scenes/gameScenes/mainScene.ts
+++ FramedDreams/src/scenes/gameScenes/mainScene.ts
@@ -28,32 +28,22 @@ export class MainScene extends AbstractModelScene {
         this._pointLight.specular = new Color3(1, 1, 1);
 
         this._scene.clearColor = new Color4(0.01568627450980392, 0.01568627450980392, 0.20392156862745098);
-        let camera = new UniversalCamera("camera1", new Vector3(0, 2, 9), this._scene);
-        camera.setTarget(new Vector3(2, 3, 10));
-        camera.speed = 0.4;
-        camera.inertia = 0.7; 
+        let camera = new UniversalCamera("camera1", new Vector3(0, 2, 8), this._scene);
+        camera.setTarget(new Vector3(0, 2, 10));
+        camera.speed = 0.2;
+        camera.inertia = 0.5; 
         camera.attachControl(true);
 
         this._scene.activeCamera = camera;
         const assumedFramesPerSecond = 60;
         const earthGravity = -9.81;
-        this._scene.gravity = new Vector3(0,  earthGravity / assumedFramesPerSecond, 0);
-        //camera.applyGravity = true;
-        camera.ellipsoid = new Vector3(1, 1, 1);
+        //this._scene.gravity = new Vector3(0, earthGravity / assumedFramesPerSecond, 0);
+        /*camera.applyGravity = true;
+        //camera.ellipsoid = new Vector3(1, 1, 1);
+        camera.ellipsoid = new Vector3(1, 2, 1); 
+        camera.ellipsoidOffset = new Vector3(0, 1, 0); 
         this._scene.collisionsEnabled = true;
-        //camera.checkCollisions = true;
-
-        const hdrTexture = new HDRCubeTexture("/meadow_2_2k.hdr", this._scene, 512);
-    	this._scene.environmentTexture = hdrTexture;
-        const skybox = MeshBuilder.CreateBox("skyBox", { size: 1000 }, this._scene);
-        const skyboxMaterial = new StandardMaterial("skyBoxMaterial", this._scene);
-        skyboxMaterial.backFaceCulling = false;
-        skyboxMaterial.reflectionTexture = hdrTexture;
-        skyboxMaterial.reflectionTexture.coordinatesMode = Texture.SKYBOX_MODE;
-        skyboxMaterial.diffuseColor = new Color3(0, 0, 0);
-        skyboxMaterial.specularColor = new Color3(0, 0, 0);
-        skybox.material = skyboxMaterial;
-        this._scene.environmentIntensity = 0.1;
+        camera.checkCollisions = true;*/
         
         await this.environment.load();
         await this._loadCharacterAssets();
@@ -108,5 +98,7 @@ export class MainScene extends AbstractModelScene {
     public goToScene0() {
         this._goToScene0();
     }
+
+    private add
 
 }
