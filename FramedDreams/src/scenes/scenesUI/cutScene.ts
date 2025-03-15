502a2f47fea3c071a2e551fddc758489fd91606f
Killian Lacaque <killian@MacBook-Air-de-Killian.local>
2025-03-15 16:11:09 +0100

    Une belle version se trouve là

--- FramedDreams/src/scenes/scenesUI/cutScene.ts
+++ FramedDreams/src/scenes/scenesUI/cutScene.ts
@@ -1,9 +1,37 @@
 import { AbstractScene } from "../baseScenes/abstractScene";
 import { Engine, FreeCamera, Vector3, Color4, AssetContainer, SceneLoader, Scene, Animation } from "@babylonjs/core";
 import { AdvancedDynamicTexture, Button, Control, TextBlock, Rectangle } from "@babylonjs/gui";
-import { MainScene } from "../gameScenes/mainScene"; // Import de la mainScene
+import { MainScene } from "../gameScenes/mainScene";
+
+// Classe pour gérer le dialogue
+class DialogueManager {
+    private _dialogueSentences: string[];
+    private _currentSentenceIndex: number = 0;
+
+    constructor(dialogueSentences: string[]) {
+        this._dialogueSentences = dialogueSentences;
+    }
+
+    public getNextSentence(): string | null {
+        if (this._currentSentenceIndex < this._dialogueSentences.length) {
+            const sentence = this._dialogueSentences[this._currentSentenceIndex];
+            this._currentSentenceIndex++;
+            return sentence;
+        } else {
+            return null; // Fin du dialogue
+        }
+    }
+
+    public reset(): void {
+        this._currentSentenceIndex = 0;
+    }
+}
 
 export class CutScene extends AbstractScene {
+
+    private _nextSceneButtonContainer: Rectangle;
+    private _dialogueManager: DialogueManager;
+    private _dialogueText: TextBlock;
 
     constructor(engine: Engine, goToMainScene: () => void) {
         super(engine);
@@ -11,75 +39,73 @@ export class CutScene extends AbstractScene {
     }
 
     private _goToMainScene: () => void;
-    private _dialogueText: TextBlock;
-    private _dialogueSentences: string[] = [
-        "Première phrase du dialogue.",
-        "Deuxième phrase, un peu plus longue.",
-        "Troisième phrase, la dernière de cette scène.",
-    ];
-    private _currentSentenceIndex: number = 0;
-    private _nextSceneButton: Button; // Ajout du bouton
 
     public async load(): Promise<void> {
         this._scene.clearColor = new Color4(0, 0, 0, 1);
         let camera = new FreeCamera("camera1", new Vector3(0, 0, 0), this._scene);
         camera.setTarget(Vector3.Zero());
 
-        // Configuration du bouton "SKIP"
-        const skipBtn = Button.CreateSimpleButton("skip", "SKIP");
-        skipBtn.width = "8%";
-        skipBtn.height = "5%";
-        skipBtn.color = "white";
-        skipBtn.fontSize = 24;
-        skipBtn.fontFamily = "Arial";
-        skipBtn.top = "2%";
-        skipBtn.thickness = 0;
-        skipBtn.verticalAlignment = Control.VERTICAL_ALIGNMENT_TOP;
-        skipBtn.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_RIGHT;
-
-        this.ui.addControl(skipBtn);
-
-        skipBtn.onPointerDownObservable.add(() => {
-            console.log("Skip button clicked."); // Ajout d'un log
-            this._scene.detachControl();
-            this._engine.displayLoadingUI();
-            console.log("Calling _goToMainScene from skip button."); // Ajout d'un log
-            this._goToMainScene();
-        });
+        // Initialise le gestionnaire de dialogue avec les phrases
+        this._dialogueManager = new DialogueManager([
+            "Où suis-je... ?",
+            "Pourquoi fait-il si sombre ?",
+            "Je devrais me souvenir... mais de quoi ?",
+            "Quelque chose m'attend... je le sens.",
+            "Il est temps d’ouvrir les yeux."
+        ]);
 
         // Configuration du texte de dialogue
         this._dialogueText = new TextBlock();
         this._dialogueText.animations = [];
-        this._dialogueText.fontFamily = "Arial";
-        this._dialogueText.fontSize = 36;
+        this._dialogueText.fontFamily = "EB Garamond";
+        this._dialogueText.fontSize = 30;
         this._dialogueText.color = "white";
         this._dialogueText.text = "";
         this._dialogueText.verticalAlignment = Control.VERTICAL_ALIGNMENT_CENTER;
         this._dialogueText.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_CENTER;
-
+        this._dialogueText.top = "-5%";
         this.ui.addControl(this._dialogueText);
 
-        // Configuration du bouton "Next Scene" (initialement caché)
-        this._nextSceneButton = Button.CreateSimpleButton("nextScene", "Go to Main Scene");
-        this._nextSceneButton.animations = []; // Initialisation explicite du tableau animations
-        this._nextSceneButton.width = "15%"; // Ajuster la largeur
-        this._nextSceneButton.height = "7%"; // Ajuster la hauteur
-        this._nextSceneButton.color = "white";
-        this._nextSceneButton.fontSize = 24;
-        this._nextSceneButton.fontFamily = "Arial";
-        this._nextSceneButton.verticalAlignment = Control.VERTICAL_ALIGNMENT_BOTTOM;
-        this._nextSceneButton.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_CENTER;
-        this._nextSceneButton.top = "-10%"; // Position en bas
-        this._nextSceneButton.alpha = 0; // Invisible initialement
-        this._nextSceneButton.thickness = 0;
-        this.ui.addControl(this._nextSceneButton);
+        // Configuration du bouton "Next Scene"
+        this._nextSceneButtonContainer = new Rectangle("nextSceneContainer");
+        this._nextSceneButtonContainer.animations = [];
+        this._nextSceneButtonContainer.width = "15%";
+        this._nextSceneButtonContainer.height = "7%";
+        this._nextSceneButtonContainer.verticalAlignment = Control.VERTICAL_ALIGNMENT_BOTTOM;
+        this._nextSceneButtonContainer.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_CENTER;
+        this._nextSceneButtonContainer.top = "-10%";
+        this._nextSceneButtonContainer.alpha = 0;
+        this._nextSceneButtonContainer.thickness = 0;
+        this.ui.addControl(this._nextSceneButtonContainer);
 
-        this._nextSceneButton.onPointerUpObservable.add(() => {
-            console.log("Go to Main Scene button clicked."); // Ajout d'un log
+        // Style du container
+        this._nextSceneButtonContainer.background = "transparent"; // Fond transparent par défaut
+
+        // Crée le TextBlock pour le texte
+        const buttonText = new TextBlock();
+        buttonText.fontFamily = "EB Garamond";
+        buttonText.text = "Se réveiller";
+        buttonText.color = "rgba(255, 255, 255, 0.5)";  // Couleur initiale du texte
+        buttonText.shadowBlur = 0;
+        buttonText.shadowColor = "rgba(255, 255, 255, 0.5)";
+        buttonText.fontSize = 24;
+        this._nextSceneButtonContainer.addControl(buttonText);
+
+        // Ajoute un effet de surbrillance au survol
+        this._nextSceneButtonContainer.onPointerEnterObservable.add(() => {
+            buttonText.color = "rgba(255, 255, 255, 1)";
+            buttonText.shadowBlur = 15 ;
+        });
+
+        this._nextSceneButtonContainer.onPointerOutObservable.add(() => {
+            buttonText.color = "rgba(255, 255, 255, 0.5)";
+            buttonText.shadowBlur = 0;
+        });
+
+        this._nextSceneButtonContainer.onPointerUpObservable.add(() => {
             this._scene.detachControl();
             this._engine.displayLoadingUI();
-            console.log("Calling _goToMainScene from Go to Main Scene button."); // Ajout d'un log
-            this._goToMainScene(); // Utilise la fonction fournie par AbstractScene
+            this._goToMainScene();
         });
 
         // Lancement du dialogue
@@ -89,28 +115,18 @@ export class CutScene extends AbstractScene {
     }
 
     private _showNextSentence = (): void => {
-        console.log("_showNextSentence() called");
-        if (this._currentSentenceIndex < this._dialogueSentences.length) {
-            const sentence = this._dialogueSentences[this._currentSentenceIndex];
-            this._currentSentenceIndex++;
+        const sentence = this._dialogueManager.getNextSentence();
 
-            console.log("Fading out current text (if any)...");
+        if (sentence) {
             this._fadeOutText(() => {
-                console.log("Text faded out. Setting new text:", sentence);
                 this._dialogueText.text = sentence;
-                console.log("Fading in new text...");
                 this._fadeInText(() => {
-                    console.log("Text faded in. Waiting 3 seconds...");
-                    // Attendre avant d'afficher la phrase suivante
                     setTimeout(() => {
-                        console.log("Waiting complete. Calling _showNextSentence()...");
                         this._showNextSentence();
-                    }, 3000); // Afficher chaque phrase pendant 3 secondes
+                    }, 3);
                 });
             });
         } else {
-            // Si toutes les phrases ont été affichées, afficher le bouton avec un fondu
-            console.log("All sentences displayed. Fading in next scene button...");
             this._fadeInButton();
         }
     }
@@ -130,14 +146,13 @@ export class CutScene extends AbstractScene {
 
         animation.setKeys(keys);
 
-        this._nextSceneButton.animations.push(animation);
-        this._scene.beginAnimation(this._nextSceneButton, 0, 30, false);
+        this._nextSceneButtonContainer.animations.push(animation);
+        this._scene.beginAnimation(this._nextSceneButtonContainer, 0, 30, false);
     }
 
-    // Fonction pour le fondu en entrée
-    private _fadeInText = (onComplete: () => void): void => {
+    private _createFadeAnimation(name: string, from: number, to: number): Animation {
         const animation = new Animation(
-            "fadeIn",
+            name,
             "alpha",
             30,
             Animation.ANIMATIONTYPE_FLOAT,
@@ -145,37 +160,29 @@ export class CutScene extends AbstractScene {
         );
 
         const keys = [];
-        keys.push({ frame: 0, value: 0 });
-        keys.push({ frame: 30, value: 1 });
+        keys.push({ frame: 0, value: from });
+        keys.push({ frame: 30, value: to });
 
         animation.setKeys(keys);
+        return animation;
+    }
 
+    // Fonction pour le fondu en entrée
+    private _fadeInText = (onComplete: () => void): void => {
+           const animation = this._createFadeAnimation("fadeIn", 0, 1);
         this._dialogueText.animations.push(animation);
         this._scene.beginAnimation(this._dialogueText, 0, 30, false, 1, onComplete);
     }
 
     // Fonction pour le fondu en sortie
     private _fadeOutText = (onComplete: () => void): void => {
-        const animation = new Animation(
-            "fadeOut",
-            "alpha",
-            30,
-            Animation.ANIMATIONTYPE_FLOAT,
-            Animation.ANIMATIONLOOPMODE_CONSTANT
-        );
-
-        const keys = [];
-        keys.push({ frame: 0, value: 1 });
-        keys.push({ frame: 30, value: 0 });
-
-        animation.setKeys(keys);
-
+             const animation = this._createFadeAnimation("fadeOut", 1, 0);
         this._dialogueText.animations.push(animation);
         this._scene.beginAnimation(this._dialogueText, 0, 30, false, 1, onComplete);
     }
+    
 
     public dispose(): void {
-        console.log("CutScene dispose() called");
         this._scene.dispose();
     }
 }
--- FramedDreams/src/scenes/scenesUI/startScene.ts
+++ FramedDreams/src/scenes/scenesUI/startScene.ts
@@ -1,19 +1,19 @@
 import { AbstractScene } from "../baseScenes/abstractScene";
-import { Engine, FreeCamera, Vector3, Color4, ParticleSystem, Texture, MeshBuilder } from "@babylonjs/core";
-import { AdvancedDynamicTexture, Button, Control, TextBlock, Rectangle } from "@babylonjs/gui";
-import { IPerfMetadataCategory } from "@babylonjs/inspector/components/actionTabs/tabs/performanceViewer/performanceViewerComponent";
+import { Engine, FreeCamera, Vector3, Color4, ParticleSystem, Texture } from "@babylonjs/core";
+import { AdvancedDynamicTexture, Button, Control, TextBlock } from "@babylonjs/gui";
 
 export class StartScene extends AbstractScene {
+
+    private _goToCutScene: () => void;
 
     constructor(engine: Engine, goToCutScene: () => void) {
         super(engine);
         this._goToCutScene = goToCutScene;
     }
 
-    private _goToCutScene: () => void;
-
     public async load(): Promise<void> {
-        this._scene.clearColor = new Color4(0.05, 0.05, 0.15, 1);
+
+        this._scene.clearColor = Color4.FromHexString("#0A1A2A");
         let camera = new FreeCamera("camera1", new Vector3(0, 0, -10), this._scene);
         camera.setTarget(Vector3.Zero());
 
@@ -24,8 +24,8 @@ export class StartScene extends AbstractScene {
         particleSystem.minEmitBox = new Vector3(-15, -15, -20);
         particleSystem.maxEmitBox = new Vector3(15, 15, 20);
 
-        particleSystem.color1 = new Color4(0.8, 0.5, 1, 0.7);
-        particleSystem.color2 = new Color4(0.5, 0.3, 0.9, 0.7);
+        particleSystem.color1 = Color4.FromHexString("#4F7CAC");
+        particleSystem.color2 = Color4.FromHexString("#8AB6D6");
 
         particleSystem.minSize = 0.1;
         particleSystem.maxSize = 0.3;
@@ -48,11 +48,14 @@ export class StartScene extends AbstractScene {
         const title = new TextBlock();
         title.fontFamily = "Rubik Puddles";
         title.text = "Framed Dreams";
-        title.color = "#dce6ff";
+        title.color = "#D6E4F0";
         title.fontSize = 90;
         title.top = "-150px";
         title.alpha = 0;
+        title.shadowBlur = 5;
+        title.shadowColor = "#EDF4FA";
         title.verticalAlignment = Control.VERTICAL_ALIGNMENT_CENTER;
+        title.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_CENTER;
         ui.addControl(title);
 
         // Animation de fondu progressive
@@ -67,23 +70,23 @@ export class StartScene extends AbstractScene {
 
         // Bouton PLAY stylisé
         const startBtn = Button.CreateSimpleButton("start", "Start");
-        startBtn.fontFamily = "Special Elite";
-        startBtn.width = 0.2;
+        startBtn.fontFamily = "EB Garamond";
+        startBtn.width = 0.15;
         startBtn.height = "60px";
-        startBtn.color = "#dce6ff";
+        startBtn.color = "#D6E4F0";
         startBtn.horizontalAlignment = Control.HORIZONTAL_ALIGNMENT_CENTER;
         
         // Transparence initiale (presque transparent)
-        startBtn.background = "rgba(38, 118, 171, 0.2)"; // Equivalent de #2676ab avec alpha à 0.2
+        startBtn.background = "rgba(27, 43, 64, 0.3)";
         startBtn.cornerRadius = 15;
-        startBtn.thickness = 0; // Retire la bordure pour un aspect plus discret
+        startBtn.thickness = 0;
         startBtn.top = "-80px";
-        startBtn.fontSize = 24;
+        startBtn.fontSize = 28;
         startBtn.verticalAlignment = Control.VERTICAL_ALIGNMENT_BOTTOM;
 
         // Augmente l'opacité au survol
-        startBtn.onPointerEnterObservable.add(() => startBtn.background = "rgba(38, 118, 171, 0.5)"); // Alpha à 0.5 au survol
-        startBtn.onPointerOutObservable.add(() => startBtn.background = "rgba(38, 118, 171, 0.2)"); // Retour à la transparence initiale
+        startBtn.onPointerEnterObservable.add(() => startBtn.background = "rgba(44, 62, 86, 0.5)"); // Alpha à 0.5 au survol
+        startBtn.onPointerOutObservable.add(() => startBtn.background = "rgba(27, 43, 64, 0.3)"); // Retour à la transparence initiale
         ui.addControl(startBtn);
 
         startBtn.onPointerDownObservable.add(() => {
