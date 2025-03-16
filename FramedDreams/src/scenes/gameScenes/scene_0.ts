5d128d16f50acb5e9f9673ad21f266e9602ab867
viksavi <viktoriia.yarmolich@univ-tlse3.fr>
2025-03-16 17:04:00 +0100

    changes

--- FramedDreams/src/scenes/gameScenes/scene_0.ts
+++ FramedDreams/src/scenes/gameScenes/scene_0.ts
@@ -41,7 +41,7 @@ export class Scene0 extends AbstractModelScene {
         this._hemiLight = new HemisphericLight("hemiLight", new Vector3(0, 1, 0), this._scene);
         this._hemiLight.intensity = 1;
 
-        this._direcLight = new DirectionalLight("direcLight", new Vector3(12, 13, -4), this._scene);
+        this._direcLight = new DirectionalLight("direcLight", new Vector3(-12, -13, 4), this._scene);
         this._direcLight.intensity = 1.5;
 
         await this.environment.load();
